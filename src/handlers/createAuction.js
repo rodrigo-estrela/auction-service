05814c273e1b6bd8eaf0fc0c27fc100fefca2170
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-25 15:27:54 -0300

    feat: add createAuction post endpoint

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,18 @@
+async function createAuction (event, context) {
+  const { title } = JSON.parse(event.body);
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString()
+  };
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = createAuction;
+
+
